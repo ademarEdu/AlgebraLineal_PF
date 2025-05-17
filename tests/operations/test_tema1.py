e3f92f559fb8782a30fef97d1a310f4da4203621
Ademar UG <aa.deleonvazquez@ugto.mx>
2025-05-17 14:11:15 -0600

    La funcion triple_dot fue agregada y soluciona el triple producto vectorial.

--- tests/operations/test_tema1.py
+++ tests/operations/test_tema1.py
@@ -5,6 +5,7 @@ import unittest
 # Importar las funciones que serán testeadas
 from src.operations.tema1.prod_esc import dot_product
 from src.operations.tema1.prod_vec import cross_product
+from src.operations.tema1.triple_prod_esc import triple_dot_product
 
 class TestsTema1(unittest.TestCase):
     """Test case para operaciones dentro de tema 1."""
@@ -18,6 +19,12 @@ class TestsTema1(unittest.TestCase):
         """Test sobre la lógica del producto vectorial."""
         result = cross_product([[4, 1, 0], [0, -2, 1]])
         self.assertEqual(result, [1, -4, -8])
+
+    def test_triple_dot(self):
+        """Test sobre la lógica del triple producto vectorial."""
+        result = triple_dot_product([[1, 3, 5], [2, -1, 0], [-3, 0, -1]])
+        print(result)
+        self.assertEqual(result, -8)
 
 # Run the tests
 if __name__ == '__main__':
